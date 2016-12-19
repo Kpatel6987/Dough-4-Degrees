53319933c6c95bee50e1874e5eb8514ee1964b5a
Yeshy Manoharan <yeshymanoharan@gmail.com>
2016-12-19 03:31:24 -0500

    register user functionality

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AlertService } from '../_services/alert.service';
+import { AuthenticationService } from '../_services/authentication.service';
+import { UserService } from '../_services/user.service';
+import { AngularFire } from 'angularfire2';
 
 @Component({
   selector: 'app-register',
@@ -9,11 +15,32 @@ export class RegisterComponent implements OnInit {
 
   model: any = {};
   loading = false;
+  returnUrl: string;
 
-  constructor() { }
+  constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private af: AngularFire,
+        private alertService: AlertService,
+        private userService: UserService
+        ) { }
 
   ngOnInit() {
+    // get return url from route parameters or default to '/'
+    //this.returnUrl = this.route.snapshot.params['returnUrl'] || '/';
   }
 
-  //TODO: Create user service and make calls to it
+  register() {
+        this.loading = true;
+        this.userService.register(this.model.firstName, this.model.lastName, this.model.email, this.model.password).then((res) => {
+            if (res.provider === 4) {
+                this.authenticationService.showNavBar(true);
+                this.router.navigate(["/home"]);
+            }
+        }).catch((err) => {
+            this.alertService.error("Sorry. Incorrect email or password");
+            this.loading = false;
+        });
+    }
 }
