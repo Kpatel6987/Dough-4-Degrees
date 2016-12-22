5e225771a10b9859dbc7ec9b34fa98e1f8933b77
Kavish Patel <Kpatel6987@users.noreply.github.com>
2016-12-21 20:45:14 -0500

    Update contacts.service.ts

--- src/app/_services/contacts.service.ts
+++ src/app/_services/contacts.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
-import { AngularFire, FirebaseAuth } from 'angularfire2';
+import { AngularFire, FirebaseAuth, FirebaseListObservable } from 'angularfire2';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
 
 @Injectable()
 export class ContactsService {
- constructor(
+
+  items: FirebaseListObservable<any>;
+  constructor(
     private af: AngularFire,
     private router: Router,
-    private auth: FirebaseAuth
+    private auth: FirebaseAuth,
+    private authenticationService: AuthenticationService,
   ) { }
 
   addContact(uid: String, firstName: String, lastName: String, email: String, number: String, relationship: String) {
     const user = this.af.database.list('contacts/' + uid);
     user.push({'firstName': firstName, 'lastName': lastName, 'email': email, 'number': number, 'relationship': relationship});
+  }
+
+  getContacts(uid: String) {
+    return this.af.database.list('contacts/' + uid);
   }
 
 }
