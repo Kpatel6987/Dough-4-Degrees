35db7c673be831f95f6c94a07b69614f0d5fc0b0
Kavish Patel <Kpatel6987@users.noreply.github.com>
2016-12-21 20:47:43 -0500

    Update my-team.component.ts

--- src/app/my-team/my-team.component.ts
+++ src/app/my-team/my-team.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ContactsService } from '../_services/contacts.service';
 import { AuthenticationService } from '../_services/authentication.service';
 import { AlertService } from '../_services/alert.service';
-
+import { FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-my-team',
@@ -11,11 +11,17 @@ import { AlertService } from '../_services/alert.service';
 })
 export class MyTeamComponent  {
 
+  contacts: FirebaseListObservable<any>;
   constructor(
     private contactsService: ContactsService,
     private authenticationService: AuthenticationService,
     private alertService: AlertService
-  ) { }
+  ) {
+    this.authenticationService.getKey().then((res) => {
+      this.uid = res;
+      this.contacts = this.contactsService.getContacts(res);
+    });
+  }
 
   model: any = { };
 
@@ -31,14 +37,10 @@ export class MyTeamComponent  {
   }
 
   onSubmit() {
-    this.authenticationService.getKey().then((res) => {
-      this.contactsService.addContact(res, this.model.firstName, this.model.lastName, this.model.email, this.model.number, this.model.relationship);
-      this.alertService.success('Contact Added', true);
-      this.model = {};
-      this.addNew = false;
-        }).catch((err) => {
-            this.alertService.error(err);
-        });
+    this.contactsService.addContact(this.uid, this.model.firstName, this.model.lastName, this.model.email, this.model.number, this.model.relationship);
+    this.alertService.success('Contact Added', true);
+    this.model = {};
+    this.addNew = false;
   }
 
 }
