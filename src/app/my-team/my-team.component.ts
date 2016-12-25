7e94ba12176292ca65ea7db070fc0038b6198b53
Yeshy Manoharan <yeshymanoharan@gmail.com>
2016-12-24 22:57:28 -0500

    displaying my team members on page

--- src/app/my-team/my-team.component.ts
+++ src/app/my-team/my-team.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ContactsService } from '../_services/contacts.service';
 import { AuthenticationService } from '../_services/authentication.service';
 import { AlertService } from '../_services/alert.service';
+import { AngularFire, FirebaseListObservable} from 'angularfire2';
 
 
 @Component({
@@ -14,13 +15,20 @@ export class MyTeamComponent  {
   constructor(
     private contactsService: ContactsService,
     private authenticationService: AuthenticationService,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private af: AngularFire
   ) { }
 
   model: any = { };
 
   private addNew: boolean = false;
   private uid: String = null;
+  private data: FirebaseListObservable<any[]>;
+  private hey: String = "yo";
+
+  ngOnInit() {
+    this.loadTeam();
+  }
 
   displayForm() {
     this.addNew = true;
@@ -41,4 +49,14 @@ export class MyTeamComponent  {
         });
   }
 
+  loadTeam() {
+    this.authenticationService.getKey().then((res) => {
+      console.log(res);
+      this.data = this.af.database.list('contacts/'+ res + '/');
+
+      console.log(this.data);
+    }).catch((err) => {
+      this.alertService.error(err);
+    });
+  }
 }
