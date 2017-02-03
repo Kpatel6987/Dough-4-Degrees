162ea0b45b38c733a62111dc82b1c7eb37010feb
Kavish Patel <kpatel6987@gmail.com>
2017-02-03 15:36:26 -0500

    Fixed login and routing

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthenticationService } from '../_services/authentication.service';
 import { Observable } from 'rxjs/Observable';
+import { AngularFire } from 'angularfire2';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/first';
 
 
 
@@ -10,15 +13,19 @@ export class AuthGuard implements CanActivate {
 
     constructor(
         private router: Router,
-        private authenticationService: AuthenticationService
+        private authenticationService: AuthenticationService,
+        private af: AngularFire
     ) { }
 
     canActivate() {
-        if (this.authenticationService.loggedIn()) {
-            this.authenticationService.showNavBar(true);
-            return true;
+      return this.af.auth.map((auth) =>  {
+        if (auth == null) {
+          this.router.navigate(['/login']);
+          return false;
+        } else {
+          this.authenticationService.showNavBar(true);
+          return true;
         }
-        this.router.navigate(['/login']);
-        return false;
-  }
+      }).first()
+    }
 }
